5162960654ed52219aeaa81c6d7291bd2be47268
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-08 15:19:28 -0500

    Simplified code

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -3,7 +3,7 @@
 function reverseString(str) {
   let reversedStr = "";
   for (let i = 0; i < str.length; i++) {
-    reversedStr = reversedStr + str[str.length - 1 - i];
+    reversedStr += str[str.length - 1 - i];
   }
   return reversedStr;
 }

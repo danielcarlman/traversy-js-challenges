8063e6d81f6f02c1b1da0034a7fe78b526c34049
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-08 12:14:47 -0500

    Removed unnecessary check and made code more readable

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,14 +1,13 @@
 // LOWER LEVEL CODE
 
 function titleCase(str) {
-  let result = "";
-  if (str.charAt(0) !== " ") result = str.charAt(0).toUpperCase();
+  let result = str.charAt(0).toUpperCase();
 
   for (let i = 1; i < str.length; i++) {
-    if (str.charAt(i - 1) === " ") {
-      result = result + str.charAt(i).toUpperCase();
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
     } else {
-      result = result + str.charAt(i).toLowerCase();
+      result += str[i].toLowerCase();
     }
   }
 

0c579a06d520a9a83063904e7d16ecf937a5f86c
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-08 10:38:13 -0500

    Solved Challenge 5

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,21 @@
-function findMaxNumber() {}
+// 1) LOW LEVEL SOLUTION
+
+function findMaxNumber(arr) {
+  let maxNum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let currentNum = arr[i];
+    if (currentNum > maxNum) {
+      [currentNum, maxNum] = [maxNum, currentNum];
+    }
+  }
+  return maxNum;
+}
+
+// 2) MATH METHOD SOLUTION
+
+// function findMaxNumber(arr) {
+//     return Math.max(...arr);
+//   }
 
 module.exports = findMaxNumber;

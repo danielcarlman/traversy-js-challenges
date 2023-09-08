e4e305c397fbe3bec8a26a7ce1a4e34023b6c558
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-08 15:17:33 -0500

    Resolved Challenge 7

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,17 @@
-function reverseString() {}
+// RESULT 1 (LOWER LEVEL)
+
+function reverseString(str) {
+  let reversedStr = "";
+  for (let i = 0; i < str.length; i++) {
+    reversedStr = reversedStr + str[str.length - 1 - i];
+  }
+  return reversedStr;
+}
+
+// RESULT 2 (BUILT-IN METHOD)
+
+// function reverseString(str) {
+//   return str.split("").reverse().join("");
+// }
 
 module.exports = reverseString;

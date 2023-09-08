3296ea0fc7205e04544db6aab353e26197ad184a
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-08 08:46:32 -0500

    Solved Challenge 3

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,9 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  if (operator === "+") return num1 + num2;
+  if (operator === "-") return num1 - num2;
+  if (operator === "*") return num1 * num2;
+  if (operator === "/") return num1 / num2;
+  throw new Error("Invalid operator");
+}
 
 module.exports = calculator;

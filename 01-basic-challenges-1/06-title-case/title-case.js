fb511f4680dc71d2f6f5eb3ffd5ac563e6ead667
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-08 11:57:48 -0500

    Resolved Challenge 6

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,33 @@
-function titleCase() {}
+// LOWER LEVEL CODE
+
+function titleCase(str) {
+  let result = "";
+  if (str.charAt(0) !== " ") result = str.charAt(0).toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str.charAt(i - 1) === " ") {
+      result = result + str.charAt(i).toUpperCase();
+    } else {
+      result = result + str.charAt(i).toLowerCase();
+    }
+  }
+
+  return result;
+}
+
+// RESULT 2
+
+// function titleCase(str) {
+//   let array = str.split(" ");
+//   let result = array
+//     .map((word) => {
+//       return (
+//         word.slice(0, 1).toUpperCase() +
+//         word.toLowerCase().slice(1, word.length)
+//       );
+//     })
+//     .join(" ");
+//   return result;
+// }
 
 module.exports = titleCase;

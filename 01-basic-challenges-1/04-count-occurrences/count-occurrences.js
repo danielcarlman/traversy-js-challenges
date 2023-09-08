80333b6960d5fd568d6aff02b1365c952d5aa5ce
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-08 09:41:01 -0500

    Solved Challenge 6

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,13 @@
-function countOccurrences() {}
+function countOccurrences(str) {
+  const vowels = new Set(["a", "e", "i", "o", "u"]);
+  console.log("vowels", vowels);
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.has(str.charAt(i))) count++;
+  }
+
+  return count;
+}
 
 module.exports = countOccurrences;
--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,12 @@
-function countVowels() {}
+function countVowels(str) {
+  const vowels = new Set(["a", "e", "i", "o", "u"]);
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.has(str.charAt(i).toLowerCase())) count++;
+  }
+
+  return count;
+}
 
 module.exports = countVowels;

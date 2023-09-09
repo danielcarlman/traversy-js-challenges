0d4aa6a27a2d20bd5003cb815937b8626403d105
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-08 21:35:59 -0500

    Solved Challenge 8

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,13 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  const regExp = /[^a-z0-9]/g;
+  const word = str.toLowerCase().replace(regExp, "");
+
+  let reverse = "";
+
+  for (i = 0; i <= word.length - 1; i++) {
+    reverse += word[word.length - i - 1];
+  }
+  return word === reverse;
+}
 
 module.exports = isPalindrome;

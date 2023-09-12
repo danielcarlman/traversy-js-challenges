884801c19c6ac29bd3517139ca47ab258e65d073
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-12 16:13:16 -0500

    Solved Challenge 9

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,17 @@
-function removeDuplicates() {}
+// WITH SET
+// function removeDuplicates(array) {
+//   return [...new Set(array)];
+// }
+
+function removeDuplicates(array) {
+  let unique = [];
+
+  for (i = 0; i < array.length; i++) {
+    if (!unique.includes(array[i])) {
+      unique.push(array[i]);
+    }
+  }
+  return unique;
+}
 
 module.exports = removeDuplicates;

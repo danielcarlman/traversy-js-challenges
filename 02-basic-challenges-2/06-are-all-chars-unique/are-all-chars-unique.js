00ac3aaaa5244e1ba874194a01a0f6148a0ce4d2
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-13 14:45:07 -0500

    Solved challenge 16

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,19 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  let currChar = "";
+  for (let i = 0; i < str.length; i++) {
+    if (currChar !== str[i]) {
+      currChar = str[i];
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+// function areAllCharactersUnique(str) {
+//   const charArr = str.split("");
+//   const charSet = [...new Set(charArr)];
+//   return charArr.join("") === charSet.join("");
+// }
 
 module.exports = areAllCharactersUnique;

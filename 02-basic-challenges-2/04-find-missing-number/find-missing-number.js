57e5aa9d0d4208376b238b92d7574c767a5d27f5
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-13 09:22:51 -0500

    Solved challenge 14

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,21 @@
-function findMissingNumber() {}
+function findMissingNumber(numbers) {
+  if (!numbers) return undefined;
+  if (!numbers.length) return 1;
+
+  let numbersSum = 0;
+  let totalSum = 0;
+  const maxNum = Math.max(...numbers);
+  const minNum = Math.min(...numbers);
+
+  for (let i = minNum; i <= maxNum; i++) {
+    totalSum += i;
+  }
+
+  for (let i = 0; i < numbers.length; i++) {
+    numbersSum += numbers[i];
+  }
+
+  return totalSum - numbersSum;
+}
 
 module.exports = findMissingNumber;

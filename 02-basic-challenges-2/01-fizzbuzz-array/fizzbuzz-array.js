0abfaf07b040d7e9f619b84dd45b9e8d58461f06
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-13 06:42:50 -0500

    Solved challenge 11

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,15 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  const array = [];
+  for (i = 1; i < num + 1; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      array.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      array.push("Fizz");
+    } else if (i % 5 === 0) array.push("Buzz");
+    else array.push(i);
+  }
+
+  return array;
+}
 
 module.exports = fizzBuzzArray;

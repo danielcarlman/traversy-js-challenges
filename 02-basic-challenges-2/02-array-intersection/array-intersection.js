51c6b3435a63ff0d8c37b29c5752ee5c5656f177
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-13 07:10:51 -0500

    Solved challenge 12

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,12 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const bigArray = arr1.length >= arr2.length ? arr1 : arr2;
+  const smallArray = bigArray === arr1 ? arr2 : arr1;
+  const intersection = [];
+
+  for (let i = 0; i < bigArray.length; i++) {
+    if (bigArray.includes(smallArray[i])) intersection.push(smallArray[i]);
+  }
+  return intersection;
+}
 
 module.exports = arrayIntersection;

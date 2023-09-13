0b323fd9a7c16fcfba3a1a0d9af1a6b7174b8eec
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-13 13:54:39 -0500

    Solved challenge 15 and fixed test bug

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,18 @@
-function findMissingLetter() {}
+function findMissingLetter(letters) {
+  if (!letters || letters.length < 2) return "";
+
+  const isUpperCase = letters[0] === letters[0].toUpperCase();
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  const initialIdx = alphabet.indexOf(letters[0].toLowerCase());
+
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i].toLowerCase() !== alphabet[initialIdx + i]) {
+      return isUpperCase
+        ? alphabet[initialIdx + i].toUpperCase()
+        : alphabet[initialIdx + i];
+    }
+  }
+}
 
 module.exports = findMissingLetter;

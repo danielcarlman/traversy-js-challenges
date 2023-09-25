a558416cfefe783c05c8430b5e4291ddaea212cd
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-25 11:25:09 -0500

    Solved challenge 17

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,18 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(string) {
+  const map = {};
+  for (const char of string) {
+    !map[char] ? (map[char] = 1) : map[char]++;
+  }
+
+  const entries = Object.entries(map);
+
+  for (const [key, value] of entries) {
+    if (value === 1) return key;
+  }
+
+  return null;
+}
+
+// NO NEED TO DO OBJECT.ENTRIES. COULD HAVE JUST LOOPED AGAIN.
 
 module.exports = findFirstNonRepeatingCharacter;

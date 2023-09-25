7f68b0a13511c6ac4db6c848211809dbdca1fd43
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-25 12:59:43 -0500

    Solved challenge 19

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,9 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(n) {
+  return `(${n[0]}${n[1]}${n[2]}) ${n[3]}${n[4]}${n[5]}-${n[6]}${n[7]}${n[8]}${n[9]}`;
+}
+
+// function formatPhoneNumber2(array) {
+//   return array.join("").replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
+// }
 
 module.exports = formatPhoneNumber;

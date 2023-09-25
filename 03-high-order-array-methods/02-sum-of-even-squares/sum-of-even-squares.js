4b1297cb7928c5e7803a016fde0ec3f3b6d7f645
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-25 14:11:46 -0500

    Solved challenge 21

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,9 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  if (!numbers.length) return 0;
+  return numbers
+    .filter((num) => num % 2 === 0)
+    .map((num) => num * num)
+    .reduce((total, cur) => total + cur, 0);
+}
 
 module.exports = sumOfEvenSquares;

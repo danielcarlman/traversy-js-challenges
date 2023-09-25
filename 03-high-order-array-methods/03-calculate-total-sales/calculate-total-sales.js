8f7b5a4d1631698eff77fee5e3da1bf75a030fad
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-25 14:22:51 -0500

    Solved challenge 22

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,10 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  const totalPrice = products.reduce(
+    (total, product) => total + product.price * product.quantity,
+    0
+  );
+
+  return totalPrice * (1 + taxRate / 100);
+}
 
 module.exports = calculateTotalSalesWithTax;

1359873af42cd4b4485db1c256c02325144835eb
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-25 12:31:48 -0500

    Solved challenge 18

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,29 @@
-function diceGameSimulation() {}
+function diceGameSimulation(numSimulations) {
+  let randomNum = () => Math.ceil(Math.random() * 6);
+  let result = "";
+  const simulations = [];
+  const odds = {
+    2: "lose",
+    3: "lose",
+    7: "win",
+    11: "win",
+    12: "lose",
+  };
+
+  for (let i = 0; i < numSimulations; i++) {
+    let dice1 = randomNum();
+    let dice2 = randomNum();
+    let sum = dice1 + dice2;
+    result = odds[sum] ? odds[sum] : "roll again";
+
+    simulations[i] = {
+      dice1,
+      dice2,
+      sum,
+      result,
+    };
+  }
+  return simulations;
+}
 
 module.exports = diceGameSimulation;

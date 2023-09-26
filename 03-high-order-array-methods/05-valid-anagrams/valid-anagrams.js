2848151b50569be87dca48fd6aff6d03bf527bbe
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-26 18:30:53 -0500

    Solved challenge 24 (using sort because high-order array methods make no sense here)

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,7 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  const sorted1 = str1.split("").sort();
+  const sorted2 = str2.split("").sort();
+  return sorted1.join("") === sorted2.join("");
+}
 
 module.exports = validAnagrams;

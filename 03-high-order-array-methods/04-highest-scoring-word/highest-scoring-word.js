ee5fb8296bf826f2f8ad47f996c3fce4dba2cbd4
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-26 16:20:09 -0500

    Solved challenge 23

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,13 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  const words = str.split(" ");
+  const scores = words.map((word) =>
+    word
+      .split("")
+      .reduce((score, letter) => score + letter.charCodeAt(0) - 96, 0)
+  );
+  const highestScore = Math.max(...scores);
+  const winnerIdx = scores.indexOf(highestScore);
+  return words[winnerIdx];
+}
 
 module.exports = highestScoringWord;

36c37d4e84f03db706215b5dc22be2105c6a1b7c
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-09-26 18:59:40 -0500

    Solved challenge 25

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator-run.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator-run.js
@@ -1,5 +1,5 @@
-const generateHashtag = require('./hashtag-generator');
+const generateHashtag = require("./hashtag-generator");
 
-const result = generateHashtag('hello world');
+const result = generateHashtag("JavaScript is awesome");
 
 console.log(result);
--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,15 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  if (!str.length || str.length > 140) return false;
+  const words = str.trim().split(" ");
+  const capitalized = words
+    .map((word) => {
+      if (word.length > 0) {
+        return word[0].toUpperCase() + word.slice(1);
+      }
+      return "";
+    })
+    .join("");
+  return "#" + capitalized;
+}
 
 module.exports = generateHashtag;

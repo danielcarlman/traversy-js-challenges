f32626ed22a47233f70634ec84d60f2f10ec3c60
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-10-02 11:39:00 -0500

    Solved challenge 27

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,23 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  const mileages = cars.map((car) => car.mileage);
+
+  const totalMileage = mileages.reduce((total, m) => total + m, 0);
+  const averageMileage = totalMileage / cars.length;
+
+  const highestMileage = Math.max(...mileages);
+  const highestMileageCar = cars.filter(
+    (car) => car.mileage === highestMileage
+  );
+
+  const lowestMileage = Math.min(...mileages);
+  const lowestMileageCar = cars.filter((car) => car.mileage === lowestMileage);
+
+  return {
+    averageMileage,
+    highestMileageCar: highestMileageCar[0],
+    lowestMileageCar: lowestMileageCar[0],
+    totalMileage,
+  };
+}
 
 module.exports = analyzeCarMileage;

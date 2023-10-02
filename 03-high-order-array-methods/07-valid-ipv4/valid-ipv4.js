08d7a76540ae44d605913c9b483849fb537b960a
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-10-02 11:06:33 -0500

    Solved challenge 26

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,7 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+  const array = str.split(".");
+  if (array.length !== 4) return false;
+  return array.every((el) => el <= 255 && el.charAt(0) !== "0");
+};
 
 module.exports = isValidIPv4;

a73dec9b22a927dd8097cfe0e5b84f356c672493
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-10-16 11:21:50 -0500

    Challenge 28

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,15 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  // `missingCharCode` is the character code of the missing character
+  const missingCharCode = arr.filter((char, index) => {
+    if (index === 0) return false;
+    const prevCharCode = arr[index - 1].charCodeAt(0);
+    const currentCharCode = char.charCodeAt(0);
+    return currentCharCode - prevCharCode > 1;
+  })[0];
+
+  return missingCharCode
+    ? String.fromCharCode(missingCharCode.charCodeAt(0) - 1)
+    : "";
+}
 
 module.exports = findMissingLetter;

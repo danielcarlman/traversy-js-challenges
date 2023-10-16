11bbef8eb9cc7e8c643478cbe6878ae2e2bb6a49
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-10-16 11:01:59 -0500

    Solved Challenge 28

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,9 @@
-function validatePassword() {}
+function validatePassword(string) {
+  const hasLength = string.length > 7;
+  const hasUpperCase = /[A-Z]/.test(string);
+  const hasLowerCase = /[a-z]/.test(string);
+  const hasDigit = /\d/.test(string);
+  return hasLength && hasUpperCase && hasLowerCase && hasDigit;
+}
 
 module.exports = validatePassword;

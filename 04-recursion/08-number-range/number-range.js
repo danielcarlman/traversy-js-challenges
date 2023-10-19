a29c522196c6b58ab0930ea763d24d1355b2314f
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-10-19 16:04:18 -0500

    Solved Challenge 36

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,7 @@
-function numberRange() {}
+function numberRange(startNum, endNum) {
+  if (startNum === endNum) return [endNum];
+  console.log(startNum);
+  return [startNum, ...[numberRange(startNum + 1, endNum)]].flat(Infinity);
+}
 
 module.exports = numberRange;

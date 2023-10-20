cc1d2208b4c85907edf722ee0e24f74caf87349d
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-10-20 10:31:05 -0500

    Solved Challenge 38

--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,3 +1,25 @@
-function permutations() {}
+function generatePermutations(string) {
+  function runPermutation(FirstChar, remainingChars) {
+    if (!remainingChars.length) {
+      permutations.push(FirstChar);
+      return;
+    }
+
+    for (let i = 0; i < remainingChars.length; i++) {
+      const newFirstChar = FirstChar + remainingChars[i];
+      const newremainingChars =
+        remainingChars.slice(0, i) + remainingChars.slice(i + 1);
+      runPermutation(newFirstChar, newremainingChars);
+    }
+  }
+
+  const permutations = [];
+  runPermutation("", string);
+  return permutations;
+}
+
+const string = "abc";
+const result = generatePermutations(string);
+console.log(result);
 
 module.exports = permutations;
